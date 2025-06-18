c6c591f93ef718e1a093f6f44d31f0e1ca56306c
a2ke5e1 <aakapurv@gmail.com>
2025-06-18 20:36:47 +0530

    Added project details screen

--- task_management_app_frontend/src/Projects.tsx
+++ task_management_app_frontend/src/Projects.tsx
@@ -1,13 +1,15 @@
 import { useParams } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 import api from "./api";
+import type { ITeam } from "./components/teams/team-card";
+import type { IProject } from "./layouts/project";
 
 function Projects() {
   const { pid } = useParams<{ pid: string }>();
 
   const { status, data: project } = useQuery({
     queryKey: [`/projects/${pid}`],
-    queryFn: async () => {
+    queryFn: async (): Promise<IProject> => {
       const data = await api.get(`/projects/${pid}`);
       return data.data.data;
     },
@@ -16,8 +18,30 @@ function Projects() {
 
   return (
     <div>
-      Test {pid}
-      <div>{JSON.stringify(project, null, 2)}</div>
+      <h1 className="text-5xl">Details</h1>
+      {status === "pending" ? (
+        <p>Loading...</p>
+      ) : status === "error" ? (
+        <p>Error loading project details.</p>
+      ) : (
+        <div className="mt-4">
+          <h2 className="text-3xl">{project.name}</h2>
+          <p className="text-lg">{project.description}</p>
+          <p className="text-sm text-gray-500">
+            Created at: {new Date(project.createdAt).toLocaleDateString()}
+          </p>
+        </div>
+      )}
+      <div className="mt-8">
+        <h2 className="text-2xl">Teams</h2>
+        <ul className="list-disc pl-5">
+          {project?.teamMembers.map((team: ITeam) => (
+            <li key={team._id} className="mt-2">
+              {team.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
